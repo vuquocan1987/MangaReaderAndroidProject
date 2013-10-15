6c4f83b69afdaf370f8487043b86ba82d6cfc390
vuquocan1987 <vuquocan1987@gmail.com>
2013-10-15 17:25:06 +0700

    newer version that is -_-

--- test/src/htmlHelper.java
+++ test/src/htmlHelper.java
@@ -3,20 +3,20 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
 import org.htmlcleaner.XPatherException;
 
 public class htmlHelper {
 	final static String siteUrl = "http://mangafox.me/directory/";
-	final static 
 	
+	String theMangaLink;
     TagNode rootNode;
-    
+    int latestPage;
 
     public htmlHelper(URL htmlPage) throws IOException
     {
-
         HtmlCleaner cleaner = new HtmlCleaner();
         rootNode = cleaner.clean(htmlPage);
     }
@@ -37,7 +37,42 @@ public class htmlHelper {
     	TagNode elements[];
     	Object firstsNodes[] = rootNode.evaluateXPath("//img[@id='image']");
     	TagNode firstNode = (TagNode)firstsNodes[0]; 
+    	theMangaLink = firstNode.getAttributeByName("src");
     	return firstNode;
+    }
+    public int latestPage() throws IOException, XPatherException{
+   
+    	Object pageList[] = rootNode.evaluateXPath("//select[@class='m']/option");
+    	latestPage =pageList.length/2-1;
+    	return latestPage;
+    }
+    public List<String> getFileList(){
+    	List<String> fileList = new ArrayList<String>();
+    	String fullPath = FilenameUtils.getFullPath(theMangaLink);
+    	String postFix = theMangaLink.substring(0,theMangaLink.length()-7);
+    	for (int i = 1; i <= latestPage; i++) {
+			theMangaLink.substring(0, theMangaLink.length()-7);
+			String fileSuffix = String.format("%03d", i) + ".jpg";
+			System.out.println(postFix+fileSuffix);
+			fileList.add(postFix+fileSuffix);
+			
+		}
+    	return fileList;
+    }
+    public List<String> getChapterList() throws IOException, XPatherException{
+    	List<String> chapterList = new ArrayList<String>();
+    	Object nodesList[] = rootNode.evaluateXPath("//select[@id='top_chapter_list']/option");
+    	
+    	TagNode tmp=(TagNode)nodesList[0];
+    	System.out.println(tmp.getAttributeByName("style"));
+    	System.out.println("you there, bro?" + nodesList.length);
+    	for (int i = 0; i < nodesList.length; i++) {
+    		tmp = (TagNode)nodesList[i];
+    		System.out.println(tmp.getName());
+    		chapterList.add(tmp.getText().toString());
+    		
+		}
+    	return chapterList;
     }
     List<TagNode> getLinksByClass(String CSSClassname)
     {
@@ -58,29 +93,25 @@ public class htmlHelper {
 
         return linkList;
     }
-    public static List<String> getPageList(String firstPage){
-    	
-    }
-    public static String binarySearch latestPage(String firstPage){
-    	
-    }
+    public static String directedMangaPage = "http://mangafox.me/manga/naruto/vTBD/c650/1.html";
     public static void main(String[] args) throws IOException,XPatherException{
   
-		htmlHelper myHTMLHelper = new htmlHelper(new URL(siteUrl));
-    	TagNode myHead = myHTMLHelper.getFirstHeadNode();
-    	String themangaLink = myHead.getAttributeByName("href");
-    	System.out.println(themangaLink);
+//		htmlHelper myHTMLHelper = new htmlHelper(new URL(siteUrl));
+//    	TagNode myHead = myHTMLHelper.getFirstHeadNode();
+//    	String themangaLink = myHead.getAttributeByName("href");
+//    	System.out.println(themangaLink);
+//    	
+//    	htmlHelper mangalink = new htmlHelper(new URL(themangaLink));
+//    	TagNode mylink = mangalink.getmangalink();
+//    	String theLink = mylink.getAttributeByName("href");
+//    	System.out.println(theLink);
     	
-    	htmlHelper mangalink = new htmlHelper(new URL(themangaLink));
-    	TagNode mylink = mangalink.getmangalink();
-    	String theLink = mylink.getAttributeByName("href");
-    	System.out.println(theLink);
-    	
-    	htmlHelper mangaPage = new htmlHelper (new URL(theLink));
+    	htmlHelper mangaPage = new htmlHelper (new URL(directedMangaPage));
     	TagNode imgTag = mangaPage.getMangaPage();
     	String themangaPage = imgTag.getAttributeByName("src");
     	System.out.println(themangaPage);
-    	
-    	List<String> myPageList= getPageList(themangaPage);
+    	System.out.println(mangaPage.latestPage());
+    	mangaPage.getFileList();
+    	mangaPage.getChapterList();
 	}
 }
